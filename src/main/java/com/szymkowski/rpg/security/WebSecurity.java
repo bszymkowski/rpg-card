1ddb752aeded73e6bb10bdb828088cb1fe331313
Szymkowski.Bartlomiej BSZ <BSZ@kmd.dk>
2018-05-14 17:58:43 +0200

    Spring OAuth2 login

--- src/main/java/com/szymkowski/rpg/security/WebSecurity.java
+++ src/main/java/com/szymkowski/rpg/security/WebSecurity.java
@@ -2,15 +2,12 @@ package com.szymkowski.rpg.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import static com.szymkowski.rpg.security.SecurityConstants.SIGN_UP_URL;
 
 @EnableWebSecurity
 @RequiredArgsConstructor(onConstructor = @_(@Autowired))
@@ -21,12 +18,18 @@ class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+        http
+                .csrf().disable().authorizeRequests()
                 .anyRequest().authenticated()
+                .and()
+                .oauth2Login()
+//                        .defaultSuccessUrl("http://localhost:8000/");
+                        .defaultSuccessUrl("/api/character?id=12")
+//                .and()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()));
+
     }
 
     @Override
