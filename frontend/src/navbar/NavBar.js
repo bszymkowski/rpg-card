49baa7c67c57df0d05b569068c7b609c0b07355f
BSZ <bsz@kmd.dk>
2018-05-17 21:45:50 +0200

    Navbar now hides if not logged in

--- frontend/src/navbar/NavBar.js
+++ frontend/src/navbar/NavBar.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {connect} from "react-redux";
 
 
-export default function NavBar() {
+function NavBar({user}) {
     return(
         <div>
-            Hallo, bin NavBar
+            {user ? "Witaj, " + user.name : ""}
         </div>
     )
-    
 }
+
+const mapStateToProps = state => {
+  const user = state.auth.user;
+  return {user};
+};
+
+const mapDispatchToprops = dispatch => {
+    return {}
+};
+
+export default connect(mapStateToProps, mapDispatchToprops)(NavBar)
