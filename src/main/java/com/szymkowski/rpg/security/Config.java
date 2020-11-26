d637391cf312adecb61fa051e00dd307597d9d30
bszymkowski <bszymkowski@users.noreply.github.com>
2020-11-27 00:58:36 +0100

    CORS #2

--- src/main/java/com/szymkowski/rpg/security/Config.java
+++ src/main/java/com/szymkowski/rpg/security/Config.java
@@ -1,23 +1,32 @@
 package com.szymkowski.rpg.security;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class Config {
-
-
+    //IMPORTANT: it has to be a normal configuration class,
+//not extending WebMvcConfigurerAdapter or other Spring Security class
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://rpg-card-front.herokuapp.com/");
-            }
-        };
-    }
+    public FilterRegistrationBean customCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://rpg-card-front.herokuapp.com/");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
 
+        //IMPORTANT #2: I didn't stress enough the importance of this line in my original answer, 
+        //but it's here where we tell Spring to load this filter at the right point in the chain
+        //(with an order of precedence higher than oauth2's filters)
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 }
